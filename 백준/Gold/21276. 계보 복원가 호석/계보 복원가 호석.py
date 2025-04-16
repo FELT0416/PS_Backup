db5a1a05bde3c6c3b90f8a37d9c65207e8242f49
FELT0416 <55340385+FELT0416@users.noreply.github.com>
2025-04-16 11:46:41 +0900

    Update 계보 복원가 호석.py

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.py
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.py
@@ -11,10 +11,10 @@ for i in range(len(names)):
     idTon[i]=names[i]
 rep=int(sys.stdin.readline())
 
-
-left = [0]*n
-right = [[]for i in range(n)]
-left2 = [[]for i in range(n)]
+# 그래프 표현하기
+left = [0]*n # 연결된 자식 수 (len(left2)값과 동일하지만 이후에 수정이 필요하므로 따로 분리)
+right = [[]for i in range(n)] # 연결된 조상
+left2 = [[]for i in range(n)] # 연결된 자식
 for i in range(rep):
     le1, ri1 = sys.stdin.readline().strip().split()
     le = nToid[le1]
@@ -24,13 +24,13 @@ for i in range(rep):
     left2[ri].append(le)
 
 toCheck=deque()
-for i in range(len(left)):
+for i in range(len(left)): # 자식이 없는 노드들을 전부 큐로 넣기
     if left[i]==0:
         toCheck.append(i)
 ord=[]
 while toCheck:
     cur = toCheck.popleft()
-    for i in right[cur]:
+    for i in right[cur]: # 해당 노드의 조상들 체크 조상이 다른 자식이 더 이상 없으면 큐에 삽입
         left[i]-=1
         if left[i]==0:
             toCheck.append(i)
@@ -48,7 +48,7 @@ for i in root:
 sys.stdout.write('\n')
 
 
-childs=set()
+childs=set() # 위상 정렬을 토대로 조상의 직계 자식을 구하기
 ans=[[]for _ in range(n)]
 for i in ord:
     childs.add(i)
@@ -56,6 +56,8 @@ for i in ord:
         if j in childs:
             childs.remove(j)
             ans[i].append(j)
+
+#요구 출력대로 출력
 
 for i in range(n):
     sys.stdout.write(idTon[i]+" "+str(len(ans[i])))
